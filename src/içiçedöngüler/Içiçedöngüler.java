3b9cde9aeca7064eb5fe6fc4f8e50177baefcb1e
Nisasu Bozkurt <bozkurtnisasu@hotmail.com>
2022-12-27 16:30:34 +0300

    Çarpim tablosu eklendi

---
+++ src/içiçedöngüler/Içiçedöngüler.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package içiçedöngüler;
+
+public class Içiçedöngüler {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i + " x " + j + " = " + (i * j) + "   ");
+            }
+            System.out.println();
+
+        }
+    }
+//Çarpım tablosu *-*
+}
